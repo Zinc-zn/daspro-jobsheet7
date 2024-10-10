1b19d5c0478d272c54c273ad0df79a2737d4d632
Zinc-zn <riskiafifudin05@gmail.com>
2024-10-10 09:43:42 +0700

    Update 1

--- SiakadFor14.java
+++ SiakadFor14.java
@@ -3,7 +3,7 @@ public class SiakadFor14{
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100, lulus=0, tidak_lulus=0;
         for (int i = 1; i <= 10; i++){
             System.out.print("Masukkan nilai mahasiswa ke-"+ i + ": ");
             nilai = input.nextDouble();
@@ -13,9 +13,17 @@ public class SiakadFor14{
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60){
+                lulus++;
+            }else{
+                tidak_lulus++;
+            }
         }
         System.out.println("Nilai tertinggi: "+tertinggi);
         System.out.println("Nilai terendah: "+terendah);
+
+        System.out.println("Jumlah mahasiswa yang lulus: "+lulus);
+        System.out.println("Jumlah mahasiswa yanng tidak lulus: "+tidak_lulus);
         
     }
 }
