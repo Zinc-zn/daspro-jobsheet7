f02ae70ef4a4cf079ae238f107b90781ef846aeb
Zinc-zn <riskiafifudin05@gmail.com>
2024-10-10 10:28:54 +0700

    Update 1

--- SiakadWhile14.java
+++ SiakadWhile14.java
@@ -11,13 +11,14 @@ public class SiakadWhile14 {
         while (i < jml){
             System.out.print("\nMasukkan nilai mahasiswa ke-" +(i+1)+ ": ");
             nilai = input.nextInt();
-
+            
             if (nilai < 0 || nilai > 100 ){
                 System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid");
                 continue;
             }   
             if (nilai > 80 && nilai <100){
                 System.out.println("Nilai mahasiswa ke-" + (i+1)+ " adalah A");
+                System.out.println("Bagus, pertahankan nilainya");
             }else if (nilai > 73 && nilai <=80){
                 System.out.println("Nilai mahasiswa ke-" + (i+1)+ " adalah B+");
             }else if (nilai > 65 && nilai <=73){
